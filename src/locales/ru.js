5210d7be0ebbb30532103b2dde2648f64b736082
ismailibnaslanbek <isma.boss93@mail.ru>
2023-12-12 18:05:46 +0300

    locales

---
+++ src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    loadSuccess: 'RSS успешно загружен',
+    errors: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      alreadyExists: 'RSS уже существует',
+    },
+  },
+};
